985dd6d8ad2c8c895969e6e595c73bbbcce4af53
nupur507 <nupurverma730@gmail.com>
2025-09-11 12:31:57 +0530

    e-comm

---
+++ e-commerce/index.js
@@ -0,0 +1,33 @@
+import { products } from "./db/product.js";
+import { createProductCard } from "./createProductCart.js";
+import { findProductInCart } from "./utils/findProductInCart.js";
+
+const productContainer = document.getElementById("products");
+const filterContainer = document.querySelector(".side-bar");
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+productContainer.addEventListener("click", (event) => {
+  const isProductInCart = findProductInCart(cart, event.target.dataset.id);
+  if (!isProductInCart) {
+    const productToAddToCart = products.filter(
+      ({ _id }) => _id === event.target.dataset.id
+    );
+    cart = [...cart, ...productToAddToCart];
+    localStorage.setItem("cart", JSON.stringify(cart));
+    const cartButton = event.target;
+    cartButton.innerHTML =
+      "Go To Cart <span class='material-icons-outlined'>shopping_cart</span>";
+  } else {
+    location.href = "cart.html";
+  }
+});
+
+filterContainer.addEventListener("click", (event) =>{
+  const updateProducts = products.filter(({rating}) =>
+  rating >= Number(event.target.dataset.rating));
+  productContainer.innerHTML = "";
+  createProductCard(updateProducts, productContainer, findProductInCart, "products");
+})
+
+createProductCard(products, productContainer, findProductInCart, "products");
